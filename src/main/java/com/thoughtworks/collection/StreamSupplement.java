aadf33c01a4c995f9c2df05f5c9871d0e127336c
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-04 23:52:23 +0800

    [StreamAPI][Mark] feat: Added methods in StreamSupplement.java

--- src/main/java/com/thoughtworks/collection/StreamSupplement.java
+++ src/main/java/com/thoughtworks/collection/StreamSupplement.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamSupplement {
 
@@ -10,15 +12,24 @@ public class StreamSupplement {
     }
 
     public List<Integer> sortFromMaxToMin(List<Integer> numbers) {
-        throw new NotImplementedException();
+        return numbers
+                .stream()
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public double getAverage(List<Integer> numbers) {
-        throw new NotImplementedException();
+        return numbers
+                .stream()
+                .mapToDouble(number -> number)
+                .average()
+                .orElse(0.0);
     }
 
     public int getMaxValue(List<Integer> numbers) {
-        throw new NotImplementedException();
+        return numbers
+                .stream()
+                .max(Integer::compare)
+                .get();
     }
-
 }
